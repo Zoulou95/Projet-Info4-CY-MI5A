5acb8035d1f32f0086a5235e66503f6a3bbaa5e2
Ziyad <98968648+cyZ-tech2@users.noreply.github.com>
2025-02-22 11:48:59 +0100

    Update connexion.php

--- connexion.php
+++ connexion.php
@@ -1,37 +1,27 @@
 <?php
-
-$utilisateur = [
-    "utilisateur1@cyu.fr" => "fzfafaz",
-    "utilisateur2@cyu.fr" => "azefa",
-    "utilisateur3@cyu.fr" => "azert",
-    "utilisateur4@cyu.fr" => "agaga",
-    "utilisateur5@cyu.fr" => "agzgzoprz2",
-    "utilisateur6@cyu.fr" => "a789R2987R2H",
-    "utilisateur7@cyu.fr" => "fzioaofoi34",
-    "utilisateur8@cyu.fr" => "aze",
-    "utilisateur9@cyu.fr" => "azezaaggagzra4",
-    "utilisateur10@cyu.fr" => "zagagaga33",
-    ];
-
+$fichier = "inscriptions.txt";
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $email = $_POST['email'] ?? '';
     $password = $_POST['password'] ?? '';
 
-   
-    if (isset($utilisateur[$email]) && $utilisateur[$email] === $password) {
-        
-        header("Location: src/search.html"); 
-        exit;
-    } else {
-        
-        echo "<script>alert('Email ou mot de passe incorrect. Veuillez réessayer.');</script>";
-        echo "<script>window.history.back();</script>";
-        exit;
+    if (($handle = fopen($fichier, "r")) !== FALSE) {
+        while (($data = fgetcsv($handle, 1000, ";", '"', "")) !== FALSE) { //Taille de la lecture à voir plus tard (dynamique plutot que statique)
+            if ($data[2] === $email && $data[4] === $password) { // Soit on le hache soit on le passe en clair (à voir)
+                fclose($handle);
+                header("Location: src/userpage.html");
+                exit;
+            }
+        }
+        fclose($handle);
     }
+
+    //Modification plus tard
+    echo "<script>alert('Email ou mot de passe incorrect. Veuillez réessayer.');</script>";
+    echo "<script>window.history.back();</script>";
+    exit;
 } else {
-    
-    header("Location: connexion.html");
+    header("Location: index.html");
     exit;
 }
 ?>
