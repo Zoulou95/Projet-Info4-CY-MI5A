1744b35eee11ac8f3e3e4c53baed026d5cbc0fa6
Ziyad <98968648+cyZ-tech2@users.noreply.github.com>
2025-03-14 17:56:12 +0100

    Add files via upload

--- connexion.php
+++ connexion.php
@@ -1,20 +1,24 @@
 <?php
-$fichier = "inscriptions.txt";
+
+$data_file = '../data/user_data.json';
+ 
+    if (file_exists($data_file)) {
+        $json_data = file_get_contents($data_file);
+        $data = json_decode($json_data, true);
+    } else {
+    	header("Location: error.php");
+	}
+
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $email = $_POST['email'] ?? '';
     $password = $_POST['password'] ?? '';
-
-    if (($handle = fopen($fichier, "r")) !== FALSE) {
-        while (($data = fgetcsv($handle, 1000, ";", '"', "")) !== FALSE) { //Taille de la lecture à voir plus tard (dynamique plutot que statique)
-            if ($data[2] === $email && $data[4] === $password) { // Soit on le hache soit on le passe en clair (à voir)
-                fclose($handle);
-                header("Location: src/userpage.html");
-                exit;
-            }
-        }
-        fclose($handle);
+    
+    if(in_array($email, $data) && in_array($password, $data)) {
+    	header("Location: userpage.php");
     }
+
+
 
     //Modification plus tard
     echo "<script>alert('Email ou mot de passe incorrect. Veuillez réessayer.');</script>";
@@ -25,4 +29,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     exit;
 }
 ?>
+
+
+
 
