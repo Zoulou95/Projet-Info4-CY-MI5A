c3f6488ce745dd3b81fd2cf1eec8f23c50a63a82
Nathan <nathanchoupin@gmail.com>
2025-03-15 21:51:27 +0100

    affichage dynamique

--- src/admin_panel.php
+++ src/admin_panel.php
@@ -1,6 +1,10 @@
-<!-- admin_panel.php : administrators' page where they can modify their privileges -->
+<?php
+// Lire le fichier JSON
+$json_data = file_get_contents('../data/user_data.json');
+$users = json_decode($json_data, true);
+?>
 
-<!DOCTYPE html> 
+<!DOCTYPE html>
 <html lang="fr">
 <head>
     <title>CyLanta</title>
@@ -23,82 +27,40 @@
             <div class="headbar_rest">
                 <a class="headbar_item" href="../index.php">Accueil</a>
                 <a class="headbar_item" href="search.php">Destinations</a>
-                <a class ="headbar_item" href="advanced_search.php">Rechercher un voyage</a>
+                <a class="headbar_item" href="advanced_search.php">Rechercher un voyage</a>
             </div>
             <div class="headbar_right">
                 <a class="headbar_my_space" href="userpage.php">Mon espace</a>
-                <a href="userpage.php"><img  class="user_img_nav" src="../assets/profile_pic/example_pfp.jpg" /></a>
+                <a href="userpage.php"><img class="user_img_nav" src="../assets/profile_pic/example_pfp.jpg" /></a>
             </div>
         </div>
 
         <!-- Admin panel -->
         <div class="user_container">
-            <div class="user">
-                    <img class="user_img" src="../assets/profile_pic/example_pfp.jpg" />
-                <div class="user_name">
-                    <p class="admin_text">Dupont Jojo</p>
-                </div>
-                <div class="user_privilege">
-                    <form class="user_privilege_form">
-                        <label class="user_privilege">
-                            Privilège :
-                            <select name="Privilege" class="user_privilege_dropdown">
-                                <option value="administrateur" selected>Administrateur</option>
-                            </select>
-                        </label>
-                    </form>
-                </div>
-            </div>
-            <div class="user">
-                    <img class="user_img" src="../assets/profile_pic/example_pfp_2.jpg" />
-                <div class="user_name">
-                    <p class="vip_text">Amélie Lefevre</p>
-                </div>
-                <div class="user_privilege">
-                    <form class="user_privilege_form">
-                        <label class="user_privilege">
-                            Privilège :
-                            <select name="Privilege" class="user_privilege_dropdown">
-                                <option value="administrateur">Administrateur</option>
-                                <option value="vip" selected>VIP</option>
-                                <option value="normal">Standard</option>
-                            </select>
-                        </label>
-                    </form>
-                </div>
-                <div class="user_ban">
-                    <form method="post" action="#">
-                        <button class="user_ban_button" type="submit">BANNIR</button>
-                    </form>
-                </div>
-            </div>
-            <div class="user">
-                    <img class="user_img" src="../assets/profile_pic/example_pfp_3.jpg" />
-                <div class="user_name">Sophie Durand
-                </div>
-                <div class="user_privilege">
-                    <form class="user_privilege_form">
-                        <label class="user_privilege">
-                            Privilège :
-                            <select name="Privilege" class="user_privilege_dropdown">
-                                <option value="administrateur">Administrateur</option>
-                                <option value="vip">VIP</option>
-                                <option value="normal" selected>Standard</option>
-                            </select>
-                        </label>
-                    </form>
-                </div>
-                <div class="user_ban">
-                    <form method="post" action="#">
-                        <button class="user_ban_button" type="submit">BANNIR</button>
-                    </form>
-                </div>
-            </div>
-            <div class="user">
-                <div class="user_name">
-                    . . .
-                </div>
-            </div>
+            <?php foreach ($users as $user): ?>
+                <form class="users">
+                    <div class="user">
+                        <img class="user_img" src="../assets/profile_pic/<?php echo $user['profile_pic']; ?>" />
+                        <div class="user_name">
+                            <p class="vip_text"><?php echo $user['name']; ?></p>
+                        </div>
+                        <div class="user_privilege">
+                            <label class="user_privilege">
+                                Privilège :
+                                <select name="Privilege" class="user_privilege_dropdown">
+                                    <option value="administrateur" <?php echo $user['role'] === 'administrateur' ? 'selected' : ''; ?>>Administrateur</option>
+                                    <option value="vip" <?php echo $user['role'] === 'vip' ? 'selected' : ''; ?>>VIP</option>
+                                    <option value="normal" <?php echo $user['role'] === 'normal' ? 'selected' : ''; ?>>Standard</option>
+                                </select>
+                            </label>
+                        </div>
+                        <div class="user_ban">
+                            <button class="user_ban_button" type="submit">BANNIR</button>
+                        </div>
+                    </div>
+                </form>
+            <?php endforeach; ?>
+        </div>
     </div>
 </body>
 </html>
