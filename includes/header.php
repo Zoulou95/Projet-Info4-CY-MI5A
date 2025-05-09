2fb2c90642035025aabd3f6414563c3e84fd1f17
5sur20 <acfiren12@gmail.com>
2025-05-09 16:10:59 +0200

    dark mode

--- includes/header.php
+++ includes/header.php
@@ -33,7 +33,6 @@ function displayHeader() {
              🌙 Mode Sombre
              </button>
               <script src="'.$path_parent.'script/darkmode.js"></script>
-              <script src="'.$path_parent.'script/formFeatures.js"></script>
         </div>';
 
     if (isset($_SESSION['user'])) {
@@ -67,74 +66,42 @@ function displayHeader() {
             <a class="headbar_item" href="#" onclick="openSignInOverlay()">Connexion</a>
             <a class="headbar_item" href="#" onclick="openSignUpOverlay()">S\'inscrire</a>
         </div>
-    
-    
-        <div class="overlay" id="signinOverlay">
-            <div class="overlay_content">
-                <span class="close_btn" onclick="closeSignInOverlay()">&times;</span>
-                <h2>Connexion</h2>
-                <form action="path_to_connexion.php" method="POST">
-                    <div class="input_wrapper">
-                        <input class="overlay_input" type="email" name="email" id="emailInputLogin" placeholder="Email" maxlength="50" required>
-                        <span class="char_counter" id="emailCounterLogin">0 / 50</span>
-                    </div>
-                    <div class="input_wrapper">
-                        <input class="overlay_input" type="password" name="password" id="passwordInputLogin" placeholder="Mot de passe" maxlength="30" required>
-                        <button type="button" class="toggle_password" onclick="togglePassword(\'passwordInputLogin\', this)">Afficher</button>
-                        <span class="char_counter" id="passwordCounterLogin">0 / 30</span>
-                    </div>
-    
+            <div class="overlay" id="signinOverlay">
+        <div class="overlay_content">
+            <span class="close_btn" onclick="closeSignInOverlay()">&times;</span>
+            <h2>Connexion</h2>
+                <form action="'.$path_parent.'src/connexion.php" method="POST">
+                    <input class="overlay_input" type="email" name="email" placeholder="Email" required>
+                    <input class="overlay_input" type="password" name="password" placeholder="Mot de passe" required>
                     <button class="overlay_button" type="submit">Se connecter</button>
-                    <p class="switch_text">
-                        Vous n\'avez pas de compte ?
+                    <p class="switch_text">' .
+                       "Vous n'avez pas de compte ?" . '
                         <a href="#" onclick="switchToSignUp()">S\'inscrire</a>
                     </p>
                 </form>
-            </div>
         </div>
-    
-        <!-- Overlay d\'Inscription -->
-        <div class="overlay" id="signupOverlay">
-            <div class="overlay_content">
-                <span class="close_btn" onclick="closeSignUpOverlay()">&times;</span>
-                <h2>Inscription</h2>
-                <form action="path_to_inscription.php" method="POST">
-                    <div class="input_wrapper">
-                        <input class="overlay_input" type="text" name="forename" id="forenameInput" placeholder="Prénom" maxlength="50" required>
-                        <span class="char_counter" id="forenameCounter">0 / 50</span>
-                    </div>
-                    <div class="input_wrapper">
-                        <input class="overlay_input" type="text" name="name" id="nameInput" placeholder="Nom" maxlength="50" required>
-                        <span class="char_counter" id="nameCounter">0 / 50</span>
-                    </div>
-    
-                    <div class="input_wrapper">
-                        <input class="overlay_input" type="email" name="email" id="emailInputSignup" placeholder="Email" maxlength="50" required>
-                        <span class="char_counter" id="emailCounterSignup">0 / 50</span>
-                    </div>
-    
-                    <div class="input_wrapper">
-                        <input class="overlay_input" type="password" name="password" id="passwordInputSignup" placeholder="Mot de passe (8 caractères minimum)" minlength="8" maxlength="30" required>
-                        <button type="button" class="toggle_password" onclick="togglePassword(\'passwordInputSignup\', this)">Afficher</button>
-                        <span class="char_counter" id="passwordCounterSignup">0 / 30</span>
-                    </div>
-    
-                    <div class="input_wrapper">
-                        <input class="overlay_input" type="tel" name="tel" id="telInput" placeholder="Numéro de téléphone" maxlength="15" required>
-                        <span class="char_counter" id="telCounter">0 / 15</span>
-                    </div>
-                    <button class="overlay_button" type="submit">S\'inscrire</button>
-                    <p class="switch_text">
-                        Vous avez déjà un compte ?
-                        <a href="#" onclick="switchToSignIn()">Se connecter</a>
-                    </p>
-                </form>
-            </div>
+    </div>
+    <div class="overlay" id="signupOverlay">
+        <div class="overlay_content">
+            <span class="close_btn" onclick="closeSignUpOverlay()">&times;</span>
+            <h2>Inscription</h2>
+            <form action="'.$path_parent.'src/inscription.php" method="POST">
+                <input class="overlay_input" type="text" name="forename" placeholder="Prénom" required>
+                <input class="overlay_input" type="text" name="name" placeholder="Nom" required>
+                <input class="overlay_input" type="email" name="email" placeholder="Email" required>
+                <input class="overlay_input" type="password" name="password" placeholder="Mot de passe (8 caractères minimum)" required>
+                <input class="overlay_input" type="tel" name="tel" placeholder="Numéro de téléphone" required>
+                <button class="overlay_button" "type="submit">S\'inscrire</button>
+                <p class="switch_text">
+                    Vous avez déjà un compte ?
+                    <a href="#" onclick="switchToSignIn()">Se connecter</a>
+                </p>
+            </form>
         </div>
-    
-        <script src="'.$path_parent.'script/registration.js"></script>';
+    </div>
+    <script src="'.$path_parent.'script/registration.js"></script>';
     }
-    
+
     echo '</div>';
 }
     ?>
